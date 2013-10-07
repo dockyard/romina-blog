01ef0544811f9beb895031d7e3ee7a7a175fdbe7
Romina Vargas <rsocci@me.com>
2013-10-07 11:30:51 -0400

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RominaBlog::Application.config.secret_key_base = '94640ee9d63c3ab65474ae3500c6c690b5a699426ac623ba4a54b8eec74fd496a1b5bccedbf8874f83937dcad764e5bfde81e6c325fb26629727357e5a399146'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RominaBlog::Application.config.session_store :cookie_store, key: '_romina-blog_session'
