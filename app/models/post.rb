d001dc004b129ded4c389ca363130b45a0c8486d
Romina Vargas <rsocci@me.com>
2013-10-16 23:18:36 -0400

    Closes #10 Post restoration test
    Add line to check page for content
    
    Add deleted posts button to root page
    
    Add routes to see all destroyed posts and restore them
    
    Add show_destroyed and restore actions, modify post uniqueness validation
    
    View for deleted posts and restore button
    
    Extract query to get destroyed posts to class method
    
    Predicate method to check if admin can make changes to a post
    
    Use of helper method for clarity
    
    Change route name and method name to destroyed
    
    Test for class method destroyed
    
    Changed file name

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   include DestroyedAt
   belongs_to :admin_user
   has_many :comments, dependent: :destroy
+
+  def self.destroyed
+    unscoped.where('destroyed_at IS NOT NULL')
+  end
 end
